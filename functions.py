1dc3af86d78c501787ac3f51ee07ab47414406af
Marcela DL <leon.ari00@hotmail.com>
2024-02-12 20:57:26 +0100

    first commit

---
+++ functions.py
@@ -0,0 +1,12 @@
+FILEPATH = (r"C:\Users\leona\OneDrive\Escritorio\Python Mega Course Build 20 Apps\app01_final_modular\todos.txt")
+
+def get_todos(filepath=FILEPATH):
+  with open(filepath, "r") as file_local:
+    todos_local = file_local.readlines()
+  return todos_local
+
+def write_todos(todos_arg, filepath=FILEPATH):
+    with open(filepath, 'w') as file:
+      file.writelines(todos_arg)  
+
+
